b305c27b5e223fa4b7619c16460276c7377432c4
Vladimit Ushakov <aurel.vu@ya.ru>
2022-11-05 23:19:33 +0300

    Initial Commit

---
+++ nike_like_tutorial_app/nike_like_tutorial_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  nike_like_tutorial_appApp.swift
+//  nike_like_tutorial_app
+//
+//  Created by Владимир Ушаков on 05.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct nike_like_tutorial_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
